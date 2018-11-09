9db812cf5ae1a8a44b22e7176441d097fbbb1259
camkirk <camdenkirkland@hotmail.com>
2018-11-08 21:08:30 -0600

    adding data files, start apiRoutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,9 @@
+let reservations = require('../data/reservations.json');
+let waitinglist = require('../data/waitinglist.json');
+module.exports = function(app){
+    //routes for.....
+    //get tables
+    //get reservations
+    //post for making reservations
+    
+}
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 let express = require('express');
 let app = express();
 
+//deal with post requests
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//routes
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+//start server
 app.listen(3000,function(){
     console.log('server listening');
     

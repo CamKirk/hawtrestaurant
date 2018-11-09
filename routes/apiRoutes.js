5cf7ea86a14a06852a543edc22110ac3a0130541
camkirk <camdenkirkland@hotmail.com>
2018-11-08 21:59:58 -0600

    CAREFUL ABOUT MYSQL

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,34 @@
+
 let reservations = require('../data/reservations.json');
 let waitinglist = require('../data/waitinglist.json');
-module.exports = function(app){
+module.exports = function(app,connection){
     //routes for.....
     //get tables - ???
     //get reservations - tables reserved
     //get waitinglist - waitlist
     //post for making reservations
-    app.get('/api/reservations', (req, res)=>{
-        res.json(reservations);
+    app.get('/api/tables', (req, res)=>{
+        console.log('hi this is the tables get route');
+
+        connection.connect(`SELECT names, id, phone FROM reservations WHERE seated = TRUE`,
+        (err,rows)=>{
+            console.log(rows);
+            res.json(rows);
+        });
+
+        let redaction = reservations.map((data)=>{
+            return({
+                name: data.name,
+                ID: data.ID
+            });
+        });
+
+        res.json(redaction);
     });
-    app.get('/api/waitinglist', (req, res)=>{
+    app.get('/api/waitlist', (req, res)=>{
         res.json(waitinglist);
     });
-    app.post('/api/reservations',(req,res)=>{
+    app.post('/api/tables',(req,res)=>{
         let reservationNumber = reservations.length;
         let customer = req.body;
 
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 let express = require('express');
 let app = express();
+let mysql = require('mysql');
+let connection = mysql.createConnection(process.env.MSQLURL||3306);
+
 
 //deal with post requests
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 //routes
-require('./routes/apiRoutes')(app);
+require('./routes/apiRoutes')(app,connection);
 require('./routes/htmlRoutes')(app);
 
 //start server
